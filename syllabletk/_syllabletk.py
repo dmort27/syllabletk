7cd282fe96f7cb92c89d5603b8070616d545ff0c
David R. Mortensen <dmortens@cs.cmu.edu>
2015-11-12 17:02:55 -0500

    Implemented more functionality

--- syllabletk/_syllabletk.py
+++ syllabletk/_syllabletk.py
@@ -1,33 +1,47 @@
 # -*- coding: utf-8 -*-
 
 import panphon
+from types import *
 
 class Syllabifier(object):
     def __init__(self):
-        self.ft = FeatureTable()
+        self.ft = panphon.FeatureTable()
 
     def son_parse(self, word):
-        self.scores = map(lambda x: self.ft.sonority(x), word)
-        self.constituents = len(word) * ' '
-        # Find nuclei
-        for i, score in enumerate(self.scores):
+        word = list(panphon.segment_text(word))
+        scores = map(lambda x: self.ft.sonority(x), word)
+        constituents = len(word) * [' ']
+        assert type(scores) == ListType
+        assert type(constituents) == ListType
+        # Find nuclei.
+        for i, score in enumerate(scores):
             if score >= 4:
-                self.constituents[i] = 'N'
-        # Construct onsets
-        for i in range(word):
-            if self.constituents[i] == 'N':
+                constituents[i] = 'N'
+        # Construct onsets.
+        for i, con in enumerate(constituents):
+            if con == 'N':
                 j = i
                 while j > 0 \
-                        and self.scores[j-1] < self.scores[j] \
-                        and self.constituents[j-i] == ' ':
+                        and scores[j-1] < scores[j] \
+                        and constituents[j-1] == ' ':
                     j -= 1
-                    self.constituents[j] == 'O'
-        # Construct codas
-        for i in range(word):
-            if self.constituents[i] = 'N':
+                    constituents[j] = 'O'
+        # Construct codas.
+        for i, con in enumerate(constituents):
+            if con == 'N':
                 j = i
-                while j < len(word) - 2 \
-                        and self.scores[j] > self.scores[j+1] \
-                        and self.constituents[j+1] == ' ':
+                while j < len(word) - 1 \
+                        and scores[j] > scores[j+1] \
+                        and constituents[j+1] == ' ':
                     j += 1
-                    self.constituents[j] == 'C'
+                    constituents[j] = 'C'
+        # Leftover final Cs must be in coda.
+        i = len(word) - 1
+        while constituents[i] == ' ' and i > 0:
+            constituents[i] = 'C'
+            i -= 1
+        # Finally, leftover Cs must be in onsets.
+        for i, con in enumerate(constituents):
+            if con == ' ':
+                constituents[i] = 'O'
+        return (word, constituents)

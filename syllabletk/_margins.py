67e099480d6e55a15b3299f9a9fb8abf35e42a5c
David R. Mortensen <dmortens@cs.cmu.edu>
2015-12-15 12:51:28 -0500

    Completed functionality for syllable margin parsing.

--- syllabletk/_margins.py
+++ syllabletk/_margins.py
@@ -20,13 +20,22 @@ class MarginMinder(object):
             self.final.update(final)
 
 
-class FixedSonoritySlicer(object):
+class WordMarginParser(object):
 
     def __init__(self):
         self.ft = panphon.FeatureTable()
 
     def _sonority_map(self, word):
         return map(self.ft.sonority, word)
+
+    def from_map(self, son_map, word):
+        return ''.join([x for (x, _) in zip(word, son_map)])
+
+    def from_reverse_map(self, son_map, word):
+        return self.from_map(son_map[::-1], word[::-1])[::-1]
+
+
+class FixedSonoritySlicer(WordMarginParser):
 
     def _initial_onset(self, son_map):
         i = 0
@@ -44,14 +53,79 @@ class FixedSonoritySlicer(object):
             i -= 1
         return cod
 
-    def from_map(self, son_map, word):
-        return ''.join([x for (x, _) in zip(word, son_map)])
+    def parse(self, word):
+        son_map = self._sonority_map(word)
+        ons_son = self._initial_onset(son_map)
+        cod_son = self._final_coda(son_map)
+        ons = self.from_map(ons_son, word)
+        cod = self.from_reverse_map(cod_son, word)
+        return (ons, cod)
 
-    def from_reverse_map(self, son_map, word):
-        return self.from_map(son_map[::-1], word[::-1])[::-1]
+
+class SonorityPeakSlicer(WordMarginParser):
+
+    def _mark_offglides(self, son_map):
+        state = 'C'
+        for i, son in enumerate(son_map):
+            if son > 7:
+                state = 'V'
+            if state == 'V' and son == 7:
+                son_map[i] = son_map[i - 1]
+            if son <= 7:
+                state == 'C'
+        return son_map
+
+    def _adjust_anom_fric_ons(self, son_map):
+        length = len(son_map)
+        i = 0
+        while i < length - 2 and son_map[i] < 5:
+            if son_map[i] in [3, 4] and son_map[i + 1] in [1, 2]:
+                son_map[i] = son_map[i] - 2
+            i += 1
+        return son_map
+
+    def _adjust_anom_fric_cod(self, son_map):
+        length = len(son_map)
+        i = length - 1
+        while i > 0 and son_map[i] < 5:
+            if son_map[i] in [3, 4] and son_map[i - 1] in [1, 2]:
+                son_map[i] = son_map[i] - 2
+            i -= 1
+        return son_map
+
+    def _initial_onset(self, son_map):
+        length = len(son_map)
+        i = 0
+        ons = []
+        if length == 1:
+            return []
+        else:
+            while i < length - 1 and son_map[i] <= son_map[i + 1]:
+                ons.append(son_map[i])
+                i += 1
+            while len(ons) > 1 and ons[-1] > 7:
+                ons.pop()
+            return ons
+
+    def _final_coda(self, son_map):
+        length = len(son_map)
+        i = length - 1
+        ons = []
+        if length == 1:
+            return []
+        else:
+            while i > 0 and son_map[i] <= son_map[i - 1]:
+                ons.insert(0, son_map[i])
+                i -= 1
+            while len(ons) > 1 and ons[0] > 7:
+                ons.pop(0)
+            return ons
 
     def parse(self, word):
         son_map = self._sonority_map(word)
+        son_map = self._mark_offglides(son_map)
+        son_map = self._adjust_anom_fric_cod(son_map)
+        son_map = self._adjust_anom_fric_ons(son_map)
         ons_son = self._initial_onset(son_map)
         cod_son = self._final_coda(son_map)
         ons = self.from_map(ons_son, word)
--- syllabletk/test_margins.py
+++ syllabletk/test_margins.py
@@ -31,5 +31,96 @@ class TestFixedSonoritySlicer(unittest.TestCase):
     def test_parsing7(self):
         self.assertEqual(self.fss.parse('trenk'), ('tr', 'nk'))
 
+
+class TestSonorityPeakSlicer(unittest.TestCase):
+
+    def setUp(self):
+        self.fss = _margins.SonorityPeakSlicer()
+
+    def test_anom_fric_ons1(self):
+        self.assertEqual(self.fss._adjust_anom_fric_ons([3, 1, 9]), [1, 1, 9])
+
+    def test_anom_fric_ons2(self):
+        self.assertEqual(self.fss._adjust_anom_fric_ons([3, 1, 9, 3, 1]), [1, 1, 9, 3, 1])
+
+    def test_anom_fric_ons3(self):
+        self.assertEqual(self.fss._adjust_anom_fric_ons([1, 3, 1, 9]), [1, 1, 1, 9])
+
+    def test_anom_fric_cod1(self):
+        self.assertEqual(self.fss._adjust_anom_fric_cod([8, 1, 3]), [8, 1, 1])
+
+    def test_anom_fric_cod2(self):
+        self.assertEqual(self.fss._adjust_anom_fric_cod([5, 9, 7, 1, 3, 1]), [5, 9, 7, 1, 1, 1])
+
+    def test_initial1(self):
+        self.assertEqual(self.fss._initial_onset([1, 1, 8, 8]), [1, 1])
+
+    def test_initial2(self):
+        self.assertEqual(self.fss._initial_onset([1, 1, 8, 8, 2]), [1, 1])
+
+    def test_initial3(self):
+        self.assertEqual(self.fss._initial_onset([1, 1, 6, 8, 1]), [1, 1, 6])
+
+    def test_initial4(self):
+        self.assertEqual(self.fss._initial_onset([1, 7]), [1])
+
+    def test_initial5(self):
+        self.assertEqual(self.fss._initial_onset([7]), [])
+
+    def test_final1(self):
+        self.assertEqual(self.fss._final_coda([8, 1]), [1])
+
+    def test_final2(self):
+        self.assertEqual(self.fss._final_coda([8, 1, 1]), [1, 1])
+
+    def test_final3(self):
+        self.assertEqual(self.fss._final_coda([1, 8, 1]), [1])
+
+    def test_final4(self):
+        self.assertEqual(self.fss._final_coda([7]), [])
+
+    def test_parsing1(self):
+        self.assertEqual(self.fss.parse('piklz'), ('p', 'z'))
+
+    def test_parsing2(self):
+        self.assertEqual(self.fss.parse('trambon'), ('tr', 'n'))
+
+    def test_parsing3(self):
+        self.assertEqual(self.fss.parse('snits'), ('sn', 'ts'))
+
+    def test_parsing4(self):
+        self.assertEqual(self.fss.parse('holtst'), ('h', 'ltst'))
+
+    def test_parsing5(self):
+        self.assertEqual(self.fss.parse('stits'), ('st', 'ts'))
+
+    def test_parsing6(self):
+        self.assertEqual(self.fss.parse('nawst'), ('n', 'st'))
+
+    def test_parsing7(self):
+        self.assertEqual(self.fss.parse('trenk'), ('tr', 'nk'))
+
+    def test_parsing8(self):
+        self.assertEqual(self.fss.parse('swist'), ('sw', 'st'))
+
+    def test_parsing9(self):
+        self.assertEqual(self.fss.parse('plizb'), ('pl', 'zb'))
+
+    def test_parsing10(self):
+        self.assertEqual(self.fss.parse('svensk'), ('sv', 'nsk'))
+
+    def test_parsing11(self):
+        self.assertEqual(self.fss.parse('strankl'), ('str', ''))
+
+    def test_parsing12(self):
+        self.assertEqual(self.fss.parse('test'), ('t', 'st'))
+
+    def test_parsing13(self):
+        self.assertEqual(self.fss.parse('sten'), ('st', 'n'))
+
+    def test_parsing14(self):
+        self.assertEqual(self.fss.parse('klp'), ('k', 'p'))
+
+
 if __name__ == '__main__':
     unittest.main()

23574c83bced1225de5ebacbde964a4c7aac1bce
David R. Mortensen <dmortens@cs.cmu.edu>
2016-01-19 11:39:40 -0500

    Added new methods using PhonoRepr

--- syllabletk/_parameterized.py
+++ syllabletk/_parameterized.py
@@ -46,6 +46,10 @@ class PhonoRepr(object):
         """Increment the object index."""
         self.i += 1
 
+    def decr(self):
+        """Decrement the oject index."""
+        self.i -= 1
+
     def before(self, i=None):
         """Return the tiers up to self.i.
 
@@ -99,78 +103,24 @@ class ParameterizedSyllabifier(object):
         self.cod_dict = {x: len(x) for x in self.attest_cod}
         self.ft = panphon.FeatureTable()
 
-    def _longest_prefix(self, phonrepr):
-        while phonrepr.string_before() in self.attest_ons:
-            phonrepr.incr()
+    def _longest_ons_prefix(self, phonr):
+        """Return the longest prefix of phonr.segs that is an attested onset.
+
+        phonr - a PhonoRepr object
+        """
+        while phonr.string_before() in self.attest_ons:
+            phonr.incr()
+        return phonr.string_before(phonr.i - 1)
 
     def _suffix_is_coda(self, phonr):
-        """Return True if segs are attested coda."""
-        pass
+        """Return True if segs form attested coda.
+
+        phonr - a PhonoRepr object
+        """
+        return phonr.string_after() in self.attest_cod
 
     def _mark_init_ons_nuc(self, phonr):
         pass
 
     def _mark_offglide(self, phonr):
         pass
-
-    def _mark_glides(self, scores, marks, nuclei):
-        """Mark glides following a nucleus as offglides (')'). The method
-        does not handle rising-sonority diphthongs.
-        """
-        for i in nuclei:
-            if i < len(marks) - 1:
-                if marks[i + 1] == ' ' and scores[i + 1] >= 7:
-                    marks[i + 1] = ')'
-        return marks
-
-    def _ons_valid_by_son(self, scores):
-        """Is onset valid according the the sonority sequencing principle?"""
-        last_score = 0
-        for score in scores:
-            if score < last_score:
-                return False
-            last_score = score
-        return True
-
-    def _cod_valid_by_son(self, scores):
-        """Is coda valid according the the sonority sequencing principle?"""
-        last_score = 10
-        for score in scores:
-            if score > last_score:
-                return False
-            last_score = score
-        return True
-
-    def _clust_valid_by_son(self, scores):
-        """If intervocalic cluster divides into licit coda and onset, return
-        index dividing them, else None.
-        """
-        for i in range(len(scores) + 1):
-            cod, ons = scores[:i], scores[i:]
-            if self._ons_valid_by_son(ons) and self._cod_valid_by_son(cod):
-                return cod, ons
-        return None
-
-    def _clust_valid_by_prec(self, segs):
-        """If intervocalic cluster divides into licit coda and onset, by
-        precedent, return index dividing them, else return None.
-        """
-        for i in range(len(segs) + 1):
-            cod, ons = segs[:i], segs[i:]
-            if ons in self.ons_dict and cod in self.cod_dict:
-                return cod, ons
-        return None
-
-    def _mark_initial_onset(self, marks, nuclei):
-        pass
-
-    def _mark_final_coda(self, marks, nulcei):
-        pass
-
-    def parse(self, word):
-        # Needs to be updated... badly.
-        word = list(panphon.segment_text(word))  # Divide into segments.
-        scores = map(lambda x: self.ft.sonority(x), word)  # Sonority scores.
-        cons = len(scores) * [' ']
-        cons, nuclei = self._mark_peaks_as_nuclei(scores, cons)
-        cons = self._mark_glides(scores, cons, nuclei)

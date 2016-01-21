319f67f6721136ff058f9a09b088ad7d7d6bc7b4
David R. Mortensen <dmortens@cs.cmu.edu>
2016-01-21 18:33:57 -0500

    Tested marking of offglides

--- syllabletk/test_parameterized.py
+++ syllabletk/test_parameterized.py
@@ -10,27 +10,31 @@ import _parameterized
 class TestParameterizedSyllabifier(unittest.TestCase):
     def setUp(self):
         ons = [('s', 'p', 'r'), ('p', 'r'), ('r'), ('p'), ('s')]
-        cod = [('l'), ('l', 'k',), ('l', 's'), ('k'), ('s')]
+        cod = [('l'), ('l', 'k',), ('l', 's'), ('k'), ('s'), ('t')]
         self.ft = panphon.FeatureTable()
         self.ps = _parameterized.ParameterizedSyllabifier((ons, cod))
 
     def test_longest_ons_prefix1(self):
         phonr = _parameterized.PhonoRepr(self.ft, 'praspol')
-        index = self.ps._longest_ons_prefix(phonr)
-        self.assertEqual(index, 2)
+        phonr = self.ps._longest_ons_prefix(phonr)
         self.assertEqual(''.join(phonr.marks), 'OON    ')
 
     def test_longest_ons_prefix1(self):
         phonr = _parameterized.PhonoRepr(self.ft, 'spraspol')
-        index = self.ps._longest_ons_prefix(phonr)
-        self.assertEqual(index, 3)
+        phonr = self.ps._longest_ons_prefix(phonr)
         self.assertEqual(''.join(phonr.marks), 'OOON    ')
 
-    def test_longest_cod_suffix(self):
+    def test_longest_cod_suffix1(self):
         phonr = _parameterized.PhonoRepr(self.ft, 'praspol')
-        index = self.ps._longest_cod_suffix(phonr)
-        self.assertEqual(index, 6)
+        phonr = self.ps._longest_cod_suffix(phonr)
         self.assertEqual(''.join(phonr.marks), '     NC')
+
+    def test_mark_offglides(self):
+        phonr = _parameterized.PhonoRepr(self.ft, 'prajvet')
+        phonr = self.ps._longest_ons_prefix(phonr)
+        phonr = self.ps._longest_cod_suffix(phonr)
+        phonr = self.ps._mark_offglides(phonr)
+        self.assertEqual(''.join(phonr.marks), 'OONG NC')
 
 if __name__ == '__main__':
     unittest.main()

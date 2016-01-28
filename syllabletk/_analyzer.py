1423f68009d58b5a7b415a63d4c08238f35b6030
David R. Mortensen <dmortens@cs.cmu.edu>
2016-01-28 14:44:34 -0500

    Squashed bugs.

--- syllabletk/_analyzer.py
+++ syllabletk/_analyzer.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 
 from panphon import FeatureTable
 from panphon import pat
+from collections import Counter
 
 
 class SyllableAnalyzer(object):
@@ -11,12 +12,14 @@ class SyllableAnalyzer(object):
         self.ft = FeatureTable()
 
     def syl_ons_obs_son(self, (ons, _, __)):
+        """SYL_ONSET_OBSTRUENT_SONORANT"""
         if self.ft.match_pattern(pat('[-son][+son]'), ons):
             return ons
         else:
             return None
 
     def syl_ons_plos_son(self, (ons, _, __)):
+        """SYL_ONSET_PLOSIVE_SONORANT"""
         if self.ft.match_pattern(pat('[-son -cont][+son]'), ons):
             return ons
         else:
@@ -69,3 +72,39 @@ class SyllableAnalyzer(object):
             return ons
         else:
             return None
+
+
+class SyllableAnalyzer2(object):
+
+    def __init__(self):
+        self.ft = FeatureTable()
+        self.feature_counter = Counter()
+        self.syllable_counter = 0
+        self.features = [
+            ('SYL_ONSET_COMPLEX_2',
+             lambda (o, _, __): self.ft.match_pattern_seq(pat('[][]'), o)),
+            ('SYL_ONSET_COMPLEX_3',
+             lambda (o, _, __): self.ft.match_pattern_seq(pat('[][][]'), o)),
+            ('SYL_ONSET_COMPLEX_4',
+             lambda (o, _, __): self.ft.match_pattern_seq(pat('[][][][]'), o)),
+            ('SYL_ONSET_OBSTRUENT_OBSTRUENT',
+             lambda (o, _, __): self.ft.match_pattern_seq(pat('[-son][-son]'), o)),
+            ('SYL_ONSET_PLOSIVE_PLOSIVE',
+             lambda (o, _, __): self.ft.match_pattern_seq(pat('[-son -cont][-son -cont]'), o)),
+            ('SYL_ONSET_FRICATIVE_FRICATIVE',
+             lambda (o, _, __): self.ft.match_pattern_seq(pat('[-son +cont][-son +cont]'), o)),
+            ('SYL_ONSET_PLOSIVE_FRICATIVE',
+             lambda (o, _, __): self.ft.match_pattern_seq(pat('[-son -cont][-son +cont]'), o)),
+            ('SYL_ONSET_FRICATIVE_PLOSIVE',
+             lambda (o, _, __): self.ft.match_pattern_seq(pat('[-son +cont][-son -cont]'), o)),
+            ('SYL_ONSET_FRICATIVE_PLOSIVE',
+             lambda (o, _, __): self.ft.match_pattern_seq(pat('[-son +cont][-son -cont]'), o)),
+            ('SYL_ONSET_SONORANT_SONORANT',
+             lambda (o, _, __): self.ft.match_pattern_seq(pat('[+son][+son]'), o)),
+        ]
+
+    def analyze_syllable(self, syl):
+        self.syllable_counter += 1
+        for name, f in self.features:
+            if f(syl):
+                self.feature_counter[name] += 1

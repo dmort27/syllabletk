885604cd8220f213a57b6a9816d5ad098fb2fed2
David R. Mortensen <dmortens@cs.cmu.edu>
2016-01-29 14:52:49 -0500

    Added syllable_sniffer.py

---
+++ syllabletk/bin/syllable_sniffer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import sys
+import yaml
+from collections import Counter
+
+import syllabletk
+
+
+def write_frequencies(outfile, initials, finals):
+    data = {'initials': dict(initials), 'finals': dict(finals)}
+    print(yaml.dump(data), file=sys.stdout)
+
+
+def main(infile, outfile):
+    sps = syllabletk.SonorityPeakSlicer()
+    initials, finals = Counter(), Counter()
+    for line in infile:
+        initial, final = sps.parse(line.strip())
+        initials[initial] += 1
+        finals[final] += 1
+    write_frequencies(outfile, initials, finals)
+
+
+if __name__ == '__main__':
+    main(sys.stdin, sys.stdout)

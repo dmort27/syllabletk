3b39100ff3d9bd44eb40059a2fef240c9ff780ef
David R. Mortensen <dmortens@cs.cmu.edu>
2016-01-29 14:53:56 -0500

    Started to draft syllabify_list.py

--- syllabletk/bin/syllabify_list.py
+++ syllabletk/bin/syllabify_list.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: 'utf-8' -*-
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
+from __future__ import division, print_function, unicode_literals
 
-import syllabletk
 import sys
+import panphon
+import syllabletk
+
+class Syllabiflow(object):
+    def __init__(self, infile, outfile):
+        self.ft = panphon.FeatureTable()
+        self.sps = syllabletk.SonorityPeakSlicer()
+        self.ps = syllabletk.ParameterizedSyllabifier()
+
+def main(infile, outfile):
+    pass
+
+if __name__ == '__main__':
+    main(sys.stdin, sys.stdout)

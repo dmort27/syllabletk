158c23d892700d4bbd244b6e39d56ae4e75a7ad4
David R. Mortensen <dmortens@cs.cmu.edu>
2016-01-30 17:32:26 -0500

    Removed error message

--- syllabletk/bin/syllable_sniffer.py
+++ syllabletk/bin/syllable_sniffer.py
@@ -10,14 +10,15 @@ import syllabletk
 
 def write_frequencies(outfile, initials, finals):
     data = {'initials': dict(initials), 'finals': dict(finals)}
-    print(yaml.dump(data), file=sys.stdout)
+    print(yaml.dump(data).encode('utf-8'), file=sys.stdout)
 
 
 def main(infile, outfile):
     sps = syllabletk.SonorityPeakSlicer()
     initials, finals = Counter(), Counter()
     for line in infile:
-        initial, final = sps.parse(line.strip())
+        # print(line.strip(), file=sys.stderr)
+        initial, final = sps.parse(line.strip().decode('utf-8'))
         initials[initial] += 1
         finals[final] += 1
     write_frequencies(outfile, initials, finals)

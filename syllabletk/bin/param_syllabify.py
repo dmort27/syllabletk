6b5dd755f0b7e0714c93e774e5754851827aa7b9
David R. Mortensen <davidmortensen@gmail.com>
2016-08-19 16:49:08 -0400

    Changes to param_syllabify.py

--- syllabletk/bin/param_syllabify.py
+++ syllabletk/bin/param_syllabify.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from __future__ import division
 from __future__ import print_function
@@ -18,6 +19,14 @@ def prettify_syllables(word):
         s = s + '({}-{}-{})'.format(o, n, c)
     return s
 
+def flatten_syllables(word):
+    s = ''
+    for o, n, c in word:
+        s += ''.join(o)
+        s += ''.join(n)
+        s += ''.join(c)
+    return s
+
 
 def main(margins):
     with open(margins) as f:
@@ -27,10 +36,13 @@ def main(margins):
     ps = ParameterizedSyllabifier((initials, finals))
     for line in sys.stdin:
         word = line.strip().decode('utf-8')
+        print('word={}'.format(word).encode('utf-8'))
         syllabified = ps.syllabify(word)
         if syllabified:
             pretty = prettify_syllables(syllabified)
+            flat = flatten_syllables(syllabified)
             print('"{}" -> {}'.format(word, pretty).encode('utf-8'), file=sys.stdout)
+            assert word.replace('ʼ', '') == flat
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Uses syllable margins as parameters to parse a list of words.')
